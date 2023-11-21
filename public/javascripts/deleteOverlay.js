90dd49944444235cb182182bdc7ae5e325f06b24
kentshenlim <kentshen_lim24@hotmail.com>
2023-11-21 20:26:23 +0800

    Implement delete overlay

---
+++ public/javascripts/deleteOverlay.js
@@ -0,0 +1,18 @@
+(() => {
+  const [binBtn, cancelBtn, deleteSection, overlaySection] = [
+    document.getElementById('bin-btn'),
+    document.getElementById('cancel-btn'),
+    document.getElementById('delete-section'),
+    document.getElementById('overlay-section'),
+  ];
+
+  binBtn.addEventListener('click', () => {
+    deleteSection.style.display = 'block';
+    overlaySection.style.display = 'block';
+  });
+
+  cancelBtn.addEventListener('click', () => {
+    deleteSection.style.display = 'none';
+    overlaySection.style.display = 'none';
+  });
+})();

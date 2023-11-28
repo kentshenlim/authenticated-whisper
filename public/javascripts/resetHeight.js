bcb51421a6a9d619d2ebce6604c6786a3587f2aa
kentshenlim <kentshen_lim24@hotmail.com>
2023-11-28 16:53:41 +0800

    Replace reset height

--- public/javascripts/resetHeight.js
+++
@@ -1,9 +0,0 @@
-// Reset height for mobile browser
-// Source: https://stackoverflow.com/a/43575432
-(() => {
-  function resetHeight() {
-    document.body.style.height = `${window.innerHeight}px`;
-  }
-  window.addEventListener('resize', resetHeight);
-  resetHeight();
-})();
